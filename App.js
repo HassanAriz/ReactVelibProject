2f6895042df555c60f3f75c307bba9525264f63a
Hassan Ariz <>
2019-11-25 14:22:40 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Text}>NANANJA  ...NANI !!!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    Text: {
+        fontSize:48,
+    }
+});
